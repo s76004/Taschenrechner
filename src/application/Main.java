ebc1d720749b43e28af1ead31ae0b21738c41d5f
heshsum <mail@heshsum.de>
2022-06-04 11:57:15 +0200

    Optimzed imports

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,10 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
