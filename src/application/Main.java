70f8c00c3e99272b667cab2b2637b81e759aa58d
heshsum <mail@heshsum.de>
2022-06-04 12:44:34 +0200

    Formatting

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,13 +11,13 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("tf.fxml"));
-            Scene scene = new Scene(root,500,500);
+            Scene scene = new Scene(root, 500, 500);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setTitle("Taschenrechner");
 
             primaryStage.show();
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
